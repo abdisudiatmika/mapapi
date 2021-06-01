8e6136726d6dfab50cb751d4e439b106243f5d3b
abdisudiatmika <sudiatmika.abdi@gmail.com>
2021-06-01 15:27:48 +0800

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,25 +1,27 @@
-<?php 
-$SERVER="sql203.epizy.com";
-$username ="epiz_28599925";
-$password="Ox3wvYVAswA46by";
-$dbname="epiz_28599925_dbmahasiswa";
+<?php
+$server ="localhost";
+$username="root";
+$password="";
+$dbname="dbmahasiswa";
 
-$conn= new mysqli($SERVER,$username,$password,$dbname);
-if ($conn->connect_error){
-    die("koneksi gagal".$conn->connect_error);
+$conn= new mysqli($server,$username,$password,$dbname);
+
+if($conn->connect_error){
+
+    die("koneksi failed".$conn->connect_error);
+
 }
-$sql="select * from t_mahasiswa";
+$sql="Select * from t_mahasiswa";
 $result=$conn->query($sql);
 $data=array();
 if($result->num_rows>0){
     while($row=$result->fetch_assoc()){
         $data[]=$row;
     }
-    }else {
+}else{
         echo"data kosong";
-
     }
-    echo json_encode($data);
-    $conn->close();
+echo json_encode($data);
+$conn->close();
 
-?>
+?> 
